e0bdf382035b2b492e55ccba8529a205c7e6ca5f
haigle <991382548@qq.com>
2021-12-05 20:54:47 +0800

    菜单权限存储

--- src/main/java/cn/haigle/virtue/common/base/repository/BaseRepository.java
+++ src/main/java/cn/haigle/virtue/common/base/repository/BaseRepository.java
@@ -2,7 +2,6 @@ package cn.haigle.virtue.common.base.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 /**
@@ -11,5 +10,5 @@ import org.springframework.data.repository.NoRepositoryBean;
  * @date 2019/1/17 11:02
  */
 @NoRepositoryBean
-public interface BaseRepository<T> extends JpaRepository<T,Long>, JpaSpecificationExecutor<T>, QuerydslPredicateExecutor<T> {
+public interface BaseRepository<T> extends JpaRepository<T,Long>, JpaSpecificationExecutor<T> {
 }
